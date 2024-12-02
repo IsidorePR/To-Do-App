dd0bb5799e4cc4baa66d96ef162f793e8ca084c8
Isidore <isidore155@gmail.com>
2024-12-02 16:57:37 +0530

    Implement the COMPLETE and EXIT button

--- guiToDo.py
+++ guiToDo.py
@@ -10,9 +10,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values =ToDoFunction.get_ToDos(), key ="Tasks",
                       enable_events = True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
 #Having label,input box, add button in one list make them show in 1 row.Use multiple list if you want to separate
 window =sg.Window('My To-Do App',
-                  layout = [[label], [input_box, add_button],[list_box, edit_button]],
+                  layout = [[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                   font=('Helvetica', 10))
 
 while True:
@@ -28,14 +34,22 @@ while True:
             ToDoFunction.write_ToDos(Tasks)
             window['Tasks'].update(values=Tasks)
         case "Edit":
-            Task_to_edit = value['Tasks'][0]
+            Task_to_edit = value['Tasks'][0]#Based on keys(Task, Tasks)and event(add,edit,complete etc)
             new_task = value["Task"]
 
             Tasks = ToDoFunction.get_ToDos()
             index = Tasks.index(Task_to_edit)
-            Tasks[index] = new_task +"\n"
+            Tasks[index] = new_task
             ToDoFunction.write_ToDos(Tasks)
             window['Tasks'].update(values=Tasks)
+        case "Complete":
+            Task_to_complete = value['Tasks'][0]
+            Tasks = ToDoFunction.get_ToDos()
+            Tasks.remove(Task_to_complete)
+            ToDoFunction.write_ToDos(Tasks)
+            window['Tasks'].update(values=Tasks)
+        case "Exit":
+            break
         case "Tasks":
             window["Task"].update(value=value["Tasks"][0])
         case sg.WINDOW_CLOSED:
